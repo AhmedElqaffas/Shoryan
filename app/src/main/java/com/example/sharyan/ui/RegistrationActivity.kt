ed99b1a58dee62539535583cd7d5ebd2ce685995
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-11-15 23:44:45 +0200

    Added functionality for the BloodTypePicker menu in RegistrationActivity.kt

--- app/src/main/java/com/example/sharyan/ui/RegistrationActivity.kt
+++ app/src/main/java/com/example/sharyan/ui/RegistrationActivity.kt
@@ -2,11 +2,18 @@ package com.example.sharyan.ui
 
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sharyan.R
 import kotlinx.android.synthetic.main.activity_registration.*
 import java.util.*
+
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +23,12 @@ class RegistrationActivity : AppCompatActivity() {
         // Setting a click listener for the birthDatePicker button
         birthDatePicker.setOnClickListener{
             pickBirthDate()
+        }
+
+        // Registering the bloodTypePicker TextView for Context Menu
+        registerForContextMenu(bloodTypePicker)
+        bloodTypePicker.setOnClickListener {
+            it.showContextMenu()
         }
 
 
@@ -35,10 +48,34 @@ class RegistrationActivity : AppCompatActivity() {
         val currentYear = c.get(Calendar.YEAR)
         val currentMonth = c.get(Calendar.MONTH)
         val currentDay = c.get(Calendar.DAY_OF_MONTH)
-        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            // Display Selected date in the Button's text
-            birthDatePicker.text = "$dayOfMonth - $monthOfYear - $year"
-        }, currentYear, currentMonth, currentDay)
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in the Button's text
+                birthDatePicker.text = "$dayOfMonth - $monthOfYear - $year"
+            },
+            currentYear,
+            currentMonth,
+            currentDay
+        )
         dpd.show()
+    }
+
+    /**
+     * ContextMenu code for selecting the appropriate blood type
+     */
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val bloodTypesList = resources.getStringArray(R.array.blood_types)
+        menu.setHeaderTitle("Select Blood Type")
+        for(i in bloodTypesList.indices)
+            menu.add(0, v.id, i, bloodTypesList[i].toString() )  //groupId, itemId, order, title
+
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        bloodTypePicker.text = item.title
+
+        return true
     }
 }
