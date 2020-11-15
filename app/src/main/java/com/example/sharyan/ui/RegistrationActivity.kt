06590c62be727bcc28d3d04338b8f0ac8ac2a8fc
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-11-15 21:33:11 +0200

    Added a birth date DatePickerDialog in RegistrationActivity.kt

--- app/src/main/java/com/example/sharyan/ui/RegistrationActivity.kt
+++ app/src/main/java/com/example/sharyan/ui/RegistrationActivity.kt
@@ -1,12 +1,37 @@
 package com.example.sharyan.ui
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sharyan.R
+import kotlinx.android.synthetic.main.activity_registration.*
+import java.util.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        // Setting a click listener for the birthDatePicker button
+        birthDatePicker.setOnClickListener{
+            pickBirthDate()
+        }
+
+    }
+
+    /**
+     * This function opens a DatePicker dialog so that the user can select his/her birth date for
+     * registration
+     */
+    fun pickBirthDate(){
+        val c = Calendar.getInstance()
+        val currentYear = c.get(Calendar.YEAR)
+        val currentMonth = c.get(Calendar.MONTH)
+        val currentDay = c.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // Display Selected date in the Button's text
+            birthDatePicker.text = "$dayOfMonth - $monthOfYear - $year"
+        }, currentYear, currentMonth, currentDay)
+        dpd.show()
     }
 }
