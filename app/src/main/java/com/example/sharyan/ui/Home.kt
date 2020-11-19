69a4d700aef6de44c442ea354af521e8f9c24758
Qaffas <aelqaffasl6@gmail.com>
2020-11-19 20:16:56 +0200

    The requests fragment is now included in the home fragment as a fragment component instead of framelayout component

--- app/src/main/java/com/example/sharyan/ui/Home.kt
+++ app/src/main/java/com/example/sharyan/ui/Home.kt
@@ -18,17 +18,9 @@ class Home : Fragment() {
     override fun onResume() {
         super.onResume()
         setToolbarText(resources.getString(R.string.home))
-        //setRequestsFragment()
     }
 
     private fun setToolbarText(text: String){
         toolbarText.text = text
     }
-
-    private fun setRequestsFragment(){
-        childFragmentManager.beginTransaction().replace(
-            R.id.requestsContainer, RequestsRecyclerFragment(),
-            "requests").commit()
-    }
-
 }
