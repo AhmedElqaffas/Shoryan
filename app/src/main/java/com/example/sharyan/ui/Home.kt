dc516c773791387192907f520809d5f0fb576ca5
Qaffas <aelqaffasl6@gmail.com>
2020-12-10 21:18:49 +0200

    Removed the unused RequestsRecyclerFragment

--- app/src/main/java/com/example/sharyan/ui/Home.kt
+++ app/src/main/java/com/example/sharyan/ui/Home.kt
@@ -5,33 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.sharyan.R
-import kotlinx.android.synthetic.main.appbar.*
+import com.example.sharyan.recyclersAdapters.RequestsRecyclerAdapter
+import kotlinx.android.synthetic.main.appbar.toolbarText
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class Home : Fragment() {
+
+    private lateinit var requestsRecyclerAdapter: RequestsRecyclerAdapter
+    private val requestsViewModel: RequestsViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initializeRecyclerViewAdapter()
+        getOngoingRequests()
+    }
+
     override fun onResume() {
         super.onResume()
         setToolbarText(resources.getString(R.string.home))
-        //setRequestsFragment()
     }
 
     private fun setToolbarText(text: String){
         toolbarText.text = text
     }
 
-    private fun setRequestsFragment(){
-        if(childFragmentManager.findFragmentByTag("requests") == null){
-            childFragmentManager.beginTransaction().replace(
-                R.id.requestsContainer, RequestsRecyclerFragment(),
-                "requests").commit()
-        }
-
+    private fun initializeRecyclerViewAdapter(){
+        requestsRecyclerAdapter = RequestsRecyclerAdapter()
+        requestsRecycler.adapter = requestsRecyclerAdapter
     }
 
+    private fun getOngoingRequests(){
+        requestsViewModel.getOngoingRequests().observe(viewLifecycleOwner,  {
+            requestsRecyclerAdapter.addRequests(it)
+        })
+    }
 }
--- app/src/main/java/com/example/sharyan/ui/MainActivity.kt
+++ app/src/main/java/com/example/sharyan/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sharyan.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
