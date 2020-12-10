9fec36d4a8c5c0b26a70a45821877a9613e4cb87
Qaffas <aelqaffasl6@gmail.com>
2020-12-10 21:18:49 +0200

    Removed the unused RequestsRecyclerFragment

--- app/src/main/java/com/example/sharyan/ui/Home.kt
+++ app/src/main/java/com/example/sharyan/ui/Home.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.sharyan.R
-import kotlinx.android.synthetic.main.appbar.*
+import com.example.sharyan.recyclersAdapters.RequestsRecyclerAdapter
+import kotlinx.android.synthetic.main.appbar.toolbarText
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class Home : Fragment() {
+
+    private lateinit var requestsRecyclerAdapter: RequestsRecyclerAdapter
+    private val requestsViewModel: RequestsViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initializeRecyclerViewAdapter()
+        getOngoingRequests()
     }
 
     override fun onResume() {
@@ -22,5 +34,16 @@ class Home : Fragment() {
 
     private fun setToolbarText(text: String){
         toolbarText.text = text
+    }
+
+    private fun initializeRecyclerViewAdapter(){
+        requestsRecyclerAdapter = RequestsRecyclerAdapter()
+        requestsRecycler.adapter = requestsRecyclerAdapter
+    }
+
+    private fun getOngoingRequests(){
+        requestsViewModel.getOngoingRequests().observe(viewLifecycleOwner,  {
+            requestsRecyclerAdapter.addRequests(it)
+        })
     }
 }
