42adfeb6c60588c8913dd4abc55c79aee213d5d9
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-12-12 10:20:25 +0200

    Added login functionality

--- app/src/main/java/com/example/sharyan/networking/RetrofitBloodDonationInterface.kt
+++ app/src/main/java/com/example/sharyan/networking/RetrofitBloodDonationInterface.kt
@@ -1,10 +1,15 @@
 package com.example.sharyan.networking
 
 import com.example.sharyan.data.DonationRequest
+import com.example.sharyan.data.User
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface RetrofitBloodDonationInterface {
 
     @GET("/requests")
     suspend fun getAllOngoingRequests(): List<DonationRequest>
+
+    @GET("/users")
+     suspend fun getAllRegisteredUsers(): List<User>
 }
--- app/src/main/java/com/example/sharyan/ui/LoginActivity.kt
+++ app/src/main/java/com/example/sharyan/ui/LoginActivity.kt
@@ -3,19 +3,37 @@ package com.example.sharyan.ui
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.sharyan.R
 import com.example.sharyan.Utility
+import com.example.sharyan.data.User
+import com.example.sharyan.networking.RetrofitBloodDonationInterface
+import com.example.sharyan.networking.RetrofitClient
+import com.example.sharyan.repos.UsersRetriever
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class LoginActivity : AppCompatActivity() {
+   lateinit var usersLoginViewModel : UsersLoginViewModel
+   var usersList : List<User> = listOf<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        usersLoginViewModel = ViewModelProvider(this).get(UsersLoginViewModel::class.java)
+
     }
 
     override fun onResume() {
@@ -71,9 +89,52 @@ class LoginActivity : AppCompatActivity() {
             Utility.hideSoftKeyboard(this@LoginActivity, view)
     }
 
-    private fun checkLogin(){
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
-    }
+    /**
+     * This method checks the phone number and password the user has entered and validates
+     * the login attempt by checking if this phone number and password exist in the registered users
+     * data in the database. If it does exist, the user login is successful and the MainActivity starts.
+     * However, if not successful then an error message appears.
+     */
+
+    private fun checkLogin() {
+        val phoneNumber = phoneEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+        if (phoneNumber.isEmpty() or password.isEmpty()) {
+            Toast.makeText(
+                this, "Please make sure to fill missing values!",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        else { // Get users list from database or cache
+            CoroutineScope(Dispatchers.Main).launch {
+                usersLoginViewModel.getAllUsers().observe(this@LoginActivity,  {
+                    var count = 0
+                    for(user in it){
+                        // phoneNumber is 1097049699  password is pass1
+                        if((user.phoneNumber == phoneNumber.toInt()) and (user.password == password)){
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                        else{
+                            count += 1
+                        }
+                        if(count == it.size) {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Error! couldn't login. Please make sure you have entered the correct phone number and password",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } })
+            }
+
+
+
+             }
+
+
+            }
+
 }
