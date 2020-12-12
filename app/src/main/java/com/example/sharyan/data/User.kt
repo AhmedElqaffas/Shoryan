96fd2dfae10d909290feee07298b622ea45a4e5c
Qaffas <aelqaffasl6@gmail.com>
2020-12-12 21:08:22 +0200

    merged develop into 'ongoing_requests_functionality'

--- app/src/main/java/com/example/sharyan/data/User.kt
+++ app/src/main/java/com/example/sharyan/data/User.kt
@@ -3,5 +3,5 @@ package com.example.sharyan.data
 import com.google.gson.annotations.SerializedName
 
 data class User(@SerializedName("_id") val id: String, val name: Name,
-                val phoneNumber : Int, val password: String)
+                val phoneNumber : String, val password: String)
 data class Name(val firstName: String)
