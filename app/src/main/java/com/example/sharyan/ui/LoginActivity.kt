e68086457c27304c8e833d0fbcac466aa80b1c6f
Qaffas <aelqaffasl6@gmail.com>
2020-12-12 13:08:25 +0200

    - Refactored Login Activity, Login ViewModel and Login Model - Added UserStateWrapper class to wrap the user returned from the model along with any error

--- app/src/main/java/com/example/sharyan/ui/LoginActivity.kt
+++ app/src/main/java/com/example/sharyan/ui/LoginActivity.kt
@@ -3,36 +3,26 @@ package com.example.sharyan.ui
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.sharyan.R
 import com.example.sharyan.Utility
-import com.example.sharyan.data.User
-import com.example.sharyan.networking.RetrofitBloodDonationInterface
-import com.example.sharyan.networking.RetrofitClient
-import com.example.sharyan.repos.UsersRetriever
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class LoginActivity : AppCompatActivity() {
-   lateinit var usersLoginViewModel : UsersLoginViewModel
-   var usersList : List<User> = listOf<User>()
+
+   private val usersLoginViewModel: UsersLoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        usersLoginViewModel = ViewModelProvider(this).get(UsersLoginViewModel::class.java)
+        //usersLoginViewModel = ViewModelProvider(this).get(UsersLoginViewModel::class.java)
 
     }
 
@@ -97,44 +87,72 @@ class LoginActivity : AppCompatActivity() {
      */
 
     private fun checkLogin() {
-        val phoneNumber = phoneEditText.text.toString().trim()
-        val password = passwordEditText.text.toString().trim()
-        if (phoneNumber.isEmpty() or password.isEmpty()) {
-            Toast.makeText(
-                this, "Please make sure to fill missing values!",
-                Toast.LENGTH_SHORT
-            ).show()
-            return
+
+        val phoneNumber = getEditTextValue(phoneEditText)
+        val password = getEditTextValue(passwordEditText)
+
+        if(!areAllInputsProvided(phoneNumber, password)){
+            displayError("من فضلك تأكّد من ادخال رقم الهاتف و كلمة السر")
         }
-        else { // Get users list from database or cache
-            CoroutineScope(Dispatchers.Main).launch {
-                usersLoginViewModel.getAllUsers().observe(this@LoginActivity,  {
-                    var count = 0
-                    for(user in it){
-                        // phoneNumber is 1097049699  password is pass1
-                        if((user.phoneNumber == phoneNumber.toInt()) and (user.password == password)){
-                            val intent = Intent(applicationContext, MainActivity::class.java)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                            startActivity(intent)
-                        }
-                        else{
-                            count += 1
-                        }
-                        if(count == it.size) {
-                            Toast.makeText(
-                                this@LoginActivity,
-                                "Error! couldn't login. Please make sure you have entered the correct phone number and password",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    } })
+
+        else {
+            verifyCredentials(phoneNumber, password)
+        }
+    }
+
+    private fun getEditTextValue(editText: EditText):String{
+        return editText.text.toString().trim()
+    }
+
+    private fun areAllInputsProvided(phoneNumber: String, password: String): Boolean{
+        return phoneNumber.isNotEmpty() and password.isNotEmpty()
+    }
+
+    private fun displayError(error: String){
+        Toast.makeText(this@LoginActivity, error, Toast.LENGTH_LONG).show()
+    }
+
+    private fun verifyCredentials(phoneNumber: String, password: String) {
+        usersLoginViewModel.verifyCredentials(phoneNumber, password)
+            .observe(this@LoginActivity, {
+                it.user?.let { openMainActivity() }
+                it.error?.let {message -> it.error
+                    displayError(message)
+                }
+            })
+    }
+
+    private fun openMainActivity(){
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    /*private fun getUsersList(): Flow<List<User>> {
+        return usersLoginViewModel.getAllUsers().asFlow()
+        /*CoroutineScope(Dispatchers.Main).launch {
+            usersLoginViewModel.getAllUsers().observe(this@LoginActivity,  {
+
             }
-
-
-
-             }
-
-
-            }
-
+        }*/
+        /*CoroutineScope(Dispatchers.Main).launch {
+            usersLoginViewModel.getAllUsers().observe(this@LoginActivity,  {
+                var count = 0
+                for(user in it){
+                    // phoneNumber is 1097049699  password is pass1
+                    if((user.phoneNumber == phoneNumber.toInt()) and (user.password == password)){
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }
+                    else{
+                        count += 1
+                    }
+                    if(count == it.size) {
+                        errorToast("خطأ في رقم الهاتف او كلمة السر")
+                    }
+                } })
+        }*/
+    }
+*/
 }
--- app/src/main/java/com/example/sharyan/ui/UsersLoginViewModel.kt
+++ app/src/main/java/com/example/sharyan/ui/UsersLoginViewModel.kt
@@ -1,8 +1,7 @@
 package com.example.sharyan.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.content.Intent
+import androidx.lifecycle.*
 import com.example.sharyan.data.User
 import com.example.sharyan.networking.RetrofitBloodDonationInterface
 import com.example.sharyan.networking.RetrofitClient
@@ -10,18 +9,41 @@ import com.example.sharyan.repos.UsersRetriever
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.collect
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class UsersLoginViewModel : ViewModel() {
+    private var userLiveData = MutableLiveData<User>()
+    private var usersList =  listOf<User>()
     private var usersListLiveData = MutableLiveData<List<User>>()
     private var bloodDonationAPI: RetrofitBloodDonationInterface = RetrofitClient
         .getRetrofitClient()
         .create(RetrofitBloodDonationInterface::class.java)
 
-    suspend fun getAllUsers(): LiveData<List<User>> {
+    /*suspend fun getAllUsers(): LiveData<List<User>> {
         CoroutineScope(Dispatchers.Default).async {
             usersListLiveData.postValue(UsersRetriever.getRequests(bloodDonationAPI))
         }.await()
 
         return usersListLiveData
+    }*/
+
+     fun verifyCredentials(phoneNumber: String, password: String)= liveData(Dispatchers.Default){
+         emit(UsersRetriever.verifyCredentials(bloodDonationAPI, phoneNumber, password))
+
+
+        /*CoroutineScope(Dispatchers.Default).async {
+            usersListLiveData.postValue(UsersRetriever.getRequests(bloodDonationAPI))
+        }.await()*/
+        /*return viewModelScope.async{
+            UsersRetriever.verifyCredentials(bloodDonationAPI)
+        }.await()*/
+
+
     }
+
+    /*fun getAllUsers() = liveData(Dispatchers.Default){
+            emit(UsersRetriever.getRequests(bloodDonationAPI))
+    }*/
 }
