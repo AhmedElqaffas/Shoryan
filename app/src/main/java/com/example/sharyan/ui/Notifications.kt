5ee78e39a1038485dfb17a7a2f5324e62405c68d
Qaffas <aelqaffasl6@gmail.com>
2020-12-14 17:33:47 +0200

    - Implemented notifications screen layout - Implemented ViewModel and repo, and added dummy data for testing

--- app/src/main/java/com/example/sharyan/ui/Notifications.kt
+++ app/src/main/java/com/example/sharyan/ui/Notifications.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.navGraphViewModels
 import com.example.sharyan.R
-import kotlinx.android.synthetic.main.appbar.*
+import com.example.sharyan.recyclersAdapters.NotificationsRecyclerAdapter
+import kotlinx.android.synthetic.main.appbar.toolbarText
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class Notifications : Fragment() {
+
+    private lateinit var notificationsRecyclerAdapter: NotificationsRecyclerAdapter
+    private val notificationsViewModel: NotificationsViewModel by navGraphViewModels(R.id.main_nav_graph)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):View? {
         // Inflate the layout for this fragment
@@ -18,9 +24,22 @@ class Notifications : Fragment() {
     override fun onResume() {
         super.onResume()
         setToolbarText(resources.getString(R.string.notifications))
+        initializeRecyclerViewAdapter()
+        showNotifications()
     }
 
     private fun setToolbarText(text: String){
         toolbarText.text = text
+    }
+
+    private fun initializeRecyclerViewAdapter(){
+        notificationsRecyclerAdapter = NotificationsRecyclerAdapter()
+        notificationsRecycler.adapter = notificationsRecyclerAdapter
+    }
+
+    private fun showNotifications(){
+        notificationsViewModel.getNotifications().observe(viewLifecycleOwner){
+            notificationsRecyclerAdapter.setNotificationsList(it)
+        }
     }
 }
