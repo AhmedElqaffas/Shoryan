78dbc131d8003ea4c07837499bb1b30c31b752a5
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-12-18 11:23:32 +0200

    Added spinner for selecting a specific Governorate in NewRequestFragment.kt

--- app/src/main/java/com/example/sharyan/ui/NewRequestFragment.kt
+++ app/src/main/java/com/example/sharyan/ui/NewRequestFragment.kt
@@ -1,20 +1,29 @@
 package com.example.sharyan.ui
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.core.view.children
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
 import com.example.sharyan.R
 import kotlinx.android.synthetic.main.appbar.*
 import kotlinx.android.synthetic.main.fragment_new_request.*
 
+
 class NewRequestFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_request, container, false)
     }
@@ -23,15 +32,15 @@ class NewRequestFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setToolbarText(resources.getString(R.string.new_request))
         setRadioGroupsMutuallyExclusive()
-        setSpinnerAdapter(governmentsSpinner, R.array.governments)
+        setSpinnerAdapter(spinnerGov, R.array.governments)
 
         //Adding click listeners for increment and decrement buttons
         setIncDecButtons()
     }
 
     private fun setIncDecButtons() {
-        incrementBloodBags.setOnClickListener {incNumOfBloodBags()}
-        decrementBloodBags.setOnClickListener {decNumOfBloodBags()}
+        incrementBloodBags.setOnClickListener { incNumOfBloodBags() }
+        decrementBloodBags.setOnClickListener { decNumOfBloodBags() }
     }
 
     private fun getCurrentBagsCount(): Int {
@@ -40,31 +49,31 @@ class NewRequestFragment : Fragment() {
 
     private fun incNumOfBloodBags() {
         var numOfCurrentBloodBags = getCurrentBagsCount()
-        if(numOfCurrentBloodBags == 99)
+        if (numOfCurrentBloodBags == 99)
             return
-        val newBagsNumber= numOfCurrentBloodBags + 1
+        val newBagsNumber = numOfCurrentBloodBags + 1
         bagsNumberEditText.setText(newBagsNumber.toString())
     }
 
     private fun decNumOfBloodBags() {
         var numOfCurrentBloodBags = getCurrentBagsCount()
-        if(numOfCurrentBloodBags == 0)
+        if (numOfCurrentBloodBags == 0)
             return
-        val newBagsNumber= numOfCurrentBloodBags - 1
+        val newBagsNumber = numOfCurrentBloodBags - 1
         bagsNumberEditText.setText(newBagsNumber.toString())
     }
 
-    private fun setToolbarText(text: String){
+    private fun setToolbarText(text: String) {
         toolbarText.text = text
     }
 
-    private fun setRadioGroupsMutuallyExclusive(){
+    private fun setRadioGroupsMutuallyExclusive() {
         plusTypesRadioGroup.children.forEach {
             it.setOnClickListener {
                 minusTypesRadioGroup.clearCheck()
             }
         }
-        minusTypesRadioGroup.children.forEach{
+        minusTypesRadioGroup.children.forEach {
             it.setOnClickListener {
                 plusTypesRadioGroup.clearCheck()
             }
@@ -83,7 +92,36 @@ class NewRequestFragment : Fragment() {
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 // Apply the adapter to the spinner
                 spinner.adapter = adapter
+                spinner.onItemSelectedListener = object : OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parentView: AdapterView<*>?,
+                        selectedItemView: View,
+                        position: Int,
+                        id: Long
+                    ) {
+                        if (position == 0) {
+                            governmentsSpinnerTextView.visibility = View.VISIBLE
+                        } else {
+                            governmentsSpinnerTextView.visibility = View.GONE
+                        }
+                    }
+
+                    override fun onNothingSelected(parentView: AdapterView<*>?) {
+
+                    }
+                }
             }
         }
+
+    }
+}
+
+class HintAdapter<String>(context: Context, resource: Int, objects: Int) :
+    ArrayAdapter<String>(context, resource, objects) {
+
+    override fun getCount(): Int {
+        val count = super.getCount()
+        // The last item will be the hint.
+        return if (count > 0) count - 1 else count
     }
 }
