48f46727831bc8fc91421abb18c28c87fac04116
Qaffas <aelqaffasl6@gmail.com>
2021-04-19 16:55:14 +0200

    Replaced Arial font with Open-Sans.

--- app/src/main/java/com/example/shoryan/ui/theme/Typography.kt
+++ app/src/main/java/com/example/shoryan/ui/theme/Typography.kt
@@ -8,57 +8,57 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.shoryan.R
 
-private val Arial = FontFamily(
-    Font(R.font.arial)
+private val OpenSans = FontFamily(
+    Font(R.font.opensans_regular)
 )
 
 val ShoryanTypography = Typography(
     h4 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W600,
         fontSize = 30.sp
     ),
     h5 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W600,
         fontSize = 24.sp
     ),
     h6 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W600,
         fontSize = 20.sp
     ),
     subtitle1 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W600,
         fontSize = 16.sp
     ),
     subtitle2 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     body1 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     body2 = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontSize = 14.sp
     ),
     button = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
     overline = TextStyle(
-        fontFamily = Arial,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.W500,
         fontSize = 12.sp
     )
