2c5cbbc1d5008b5a97c02033828bfc645ce242a0
Ahmed Mamdouh <mamdu7x@gmail.com>
2021-05-21 18:53:05 +0200

    Added additional logic in the AccountInfoFragment.kt. The form now contains initial values which are the current user's information.

--- app/src/main/java/com/example/shoryan/ui/AccountInfoFragment.kt
+++ app/src/main/java/com/example/shoryan/ui/AccountInfoFragment.kt
@@ -20,17 +20,20 @@ import androidx.navigation.navGraphViewModels
 import com.example.shoryan.AndroidUtility
 import com.example.shoryan.R
 import com.example.shoryan.data.BirthDate
+import com.example.shoryan.data.ProfileResponse
 import com.example.shoryan.databinding.FragmentAccountInfoBinding
 import com.example.shoryan.getStringWithoutAdditionalSpaces
 import com.example.shoryan.interfaces.LoadingFragmentHolder
+import com.example.shoryan.repos.ProfileRepo
 import com.example.shoryan.repos.TokensRefresher
+import com.example.shoryan.viewmodels.AccountInfoViewModel
 import com.example.shoryan.viewmodels.LocationPickerViewModel
-import com.example.shoryan.viewmodels.RegistrationViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -38,35 +41,18 @@ import kotlinx.coroutines.withContext
 import java.util.*
 
 
-// the fragment initialization parameters, e.g. zs
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
-
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var navController: NavController
     private var _binding: FragmentAccountInfoBinding? = null
     private val binding get() = _binding!!
     private lateinit var locationPickerViewModel: LocationPickerViewModel
-    private val registrationViewModel: RegistrationViewModel by navGraphViewModels(R.id.main_nav_graph)
-    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val accountInfoViewModel: AccountInfoViewModel by navGraphViewModels(R.id.main_nav_graph)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
         initializeLocationPickerViewModel()
-        initializeUserLocation()
     }
 
 
@@ -76,7 +62,7 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentAccountInfoBinding.inflate(inflater, container, false)
-        _binding!!.viewmodel = registrationViewModel
+        _binding!!.viewmodel = accountInfoViewModel
         _binding!!.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
@@ -119,11 +105,24 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
             openLocationPicker()
         }
 
-        // Setting the textview of the address with the current location of the user if it exists
+        // Setting the textview of the address with the location chosen by the user
         setAddress()
+
+        // Get user's current profile information
+        getUserCurrentProfileInfo()
 
         // Observing the viewmodel events
         observeViewModelEvents()
+    }
+
+    /**
+     * This functions fetches the current information of the profile of the user and uses it to
+     * fill the initial values for the account info form
+     */
+    private fun getUserCurrentProfileInfo() {
+        GlobalScope.launch {
+                accountInfoViewModel.getUserProfileData()
+        }
     }
 
     /**
@@ -132,81 +131,6 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
     private fun initializeLocationPickerViewModel(){
         locationPickerViewModel = ViewModelProvider(requireActivity())
             .get(LocationPickerViewModel::class.java)
-    }
-
-    private fun initializeUserLocation(){
-        if(isNoLocationAlreadyStored()){
-            askUserForLocation()
-        }
-    }
-
-    private fun isNoLocationAlreadyStored() = locationPickerViewModel.getCurrentSavedAddress().isEmpty()
-
-    private fun askUserForLocation(){
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
-            requireActivity()
-        )
-        if (!isLocationPermissionGranted()){
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-        }
-        else {
-            getUserLastKnownLocation()
-        }
-    }
-
-    private fun isLocationPermissionGranted() =
-        (ContextCompat.checkSelfPermission(
-            requireActivity(),
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-                == PackageManager.PERMISSION_GRANTED)
-
-    // What happens when users accept or deny accessing their location
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (isLocationPermissionGranted()){
-                getUserLastKnownLocation()
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getUserLastKnownLocation(){
-        fusedLocationProviderClient?.lastLocation?.addOnSuccessListener(requireActivity()) { location: Location? ->
-            if(location != null){
-                getUserAddressFromLocation(location)
-            }
-            else{
-                Toast.makeText(requireContext(), R.string.location_failed, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-
-    /**
-     * Takes the location of the user and uses reverse geocoding to find the address.
-     * This method stores both the location and the address in the viewmodel
-     */
-    private fun getUserAddressFromLocation(location: Location){
-        val locationLatLng = LatLng(location.latitude, location.longitude)
-        locationPickerViewModel.locationLatLng = locationLatLng
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO){
-                val address = locationPickerViewModel.getAddressFromLatLng(
-                    requireActivity(),
-                    locationLatLng
-                )
-                address?.let {
-                    locationPickerViewModel.setLocation(it)
-                }
-            }
-        }
     }
 
     /**
@@ -221,7 +145,7 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
         val dpd = DatePickerDialog(
             requireActivity(),
             { _, year, monthOfYear, dayOfMonth ->
-                registrationViewModel.setBirthDate(BirthDate(year, monthOfYear + 1, dayOfMonth))
+                accountInfoViewModel.setBirthDate(BirthDate(year, monthOfYear + 1, dayOfMonth))
             },
             currentYear,
             currentMonth,
@@ -260,10 +184,10 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         if(item.groupId == 0){
-            registrationViewModel.setBloodType(item.title.toString())
+            accountInfoViewModel.setBloodType(item.title.toString())
         }
         else if(item.groupId == 1){
-            registrationViewModel.setGender(item.title.toString())
+            accountInfoViewModel.setGender(item.title.toString())
         }
         return true
     }
@@ -274,38 +198,23 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
 
     private fun setAddress(){
         locationPickerViewModel.locationStringLiveData.observe(viewLifecycleOwner){
-            registrationViewModel.setAddress(locationPickerViewModel.getLocation())
+            accountInfoViewModel.setAddress(locationPickerViewModel.getLocation())
             binding.addressEditText.setText(it)
         }
     }
 
     private fun observeViewModelEvents(){
-        registrationViewModel.eventsFlow.onEach {
+        accountInfoViewModel.eventsFlow.onEach {
             when(it){
-                is RegistrationViewModel.RegistrationViewEvent.ShowSnackBarFromString -> showSnackbar(it.text)
-                is RegistrationViewModel.RegistrationViewEvent.ShowSnackBarFromResource -> showSnackbar(resources.getString(it.textResourceId))
-                RegistrationViewModel.RegistrationViewEvent.OpenSMSFragment -> goToSMSFragment()
-                RegistrationViewModel.RegistrationViewEvent.ToggleLoadingIndicator -> toggleLoadingIndicator()
+                is AccountInfoViewModel.RegistrationViewEvent.ShowSnackBarFromString -> showSnackbar(it.text)
+                is AccountInfoViewModel.RegistrationViewEvent.ShowSnackBarFromResource -> showSnackbar(resources.getString(it.textResourceId))
+                AccountInfoViewModel.RegistrationViewEvent.ToggleLoadingIndicator -> toggleLoadingIndicator()
             }
         }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     private fun showSnackbar(message: String){
         AndroidUtility.displaySnackbarMessage(binding.rootLayout, message, Snackbar.LENGTH_LONG)
-    }
-
-    private fun onSuccessfulRegistration(accessToken: String, refreshToken: String){
-        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
-            TokensRefresher.saveTokens(accessToken, refreshToken, requireContext())
-            goToSMSFragment()
-        }
-    }
-
-    private fun goToSMSFragment(){
-        val phoneNumber = binding.registrationPhoneEditText.getStringWithoutAdditionalSpaces()
-        val registrationBundle = bundleOf("phoneNumber" to phoneNumber,
-            "registrationQuery" to registrationViewModel.createUserRegistrationQuery())
-        navController.navigate(R.id.action_registrationFragment_to_SMSFragment, registrationBundle)
     }
 
     private fun toggleLoadingIndicator(){
@@ -317,25 +226,6 @@ class AccountInfoFragment : Fragment(), LoadingFragmentHolder {
     }
 
     override fun onLoadingFragmentDismissed() {
-        registrationViewModel.cancelRegistrationProcess()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountInfoFragment.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        accountInfoViewModel.cancelRegistrationProcess()
     }
 }
--- app/src/main/java/com/example/shoryan/viewmodels/AccountInfoViewModel.kt
+++ app/src/main/java/com/example/shoryan/viewmodels/AccountInfoViewModel.kt
@@ -1,6 +1,154 @@
 package com.example.shoryan.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.widget.EditText
+import androidx.lifecycle.*
+import com.example.shoryan.AndroidUtility
+import com.example.shoryan.InputValidator
+import com.example.shoryan.R
+import com.example.shoryan.data.*
+import com.example.shoryan.networking.RetrofitBloodDonationInterface
+import com.example.shoryan.networking.RetrofitClient
+import com.example.shoryan.removeAdditionalSpaces
+import com.example.shoryan.repos.ProfileRepo
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class AccountInfoViewModel : ViewModel() {
+    sealed class RegistrationViewEvent {
+        data class ShowSnackBarFromResource(val textResourceId: Int) : RegistrationViewEvent()
+        data class ShowSnackBarFromString(val text: String) : RegistrationViewEvent()
+        object OpenSMSFragment : RegistrationViewEvent()
+        object ToggleLoadingIndicator : RegistrationViewEvent()
+    }
+
+    private val TAG = javaClass.simpleName
+    private var bloodDonationAPI: RetrofitBloodDonationInterface = RetrofitClient
+        .getRetrofitClient()
+        .create(RetrofitBloodDonationInterface::class.java)
+    private val _bloodType = MutableLiveData<BloodType>(BloodType.APositive)
+    val bloodType: LiveData<BloodType> = _bloodType
+    private val _gender = MutableLiveData<Gender>(Gender.None_EN)
+    val gender: LiveData<Gender> = _gender
+    private val _birthDate = MutableLiveData<BirthDate?>(null)
+    val birthDateString: LiveData<String> = Transformations.map(_birthDate) {
+        when (it) {
+            null -> "DD-MM-YYYY"
+            else -> "${it.day}/${it.month}/${it.year}"
+        }
+    }
+    private val _addressLiveData = MutableLiveData<Location?>()
+    val addressLiveData: LiveData<Location?> = _addressLiveData
+
+    private var _firstName = MutableLiveData("")
+    private var _lastName = MutableLiveData("")
+    private var _password = MutableLiveData("")
+    val firstName: LiveData<String> = _firstName
+    val lastName: LiveData<String> = _lastName
+
+    val shouldShowFirstNameError: LiveData<Boolean> = Transformations.map(_firstName) {
+        it != "" && !isNameValid(it)
+    }
+    val shouldShowLastNameError: LiveData<Boolean> = Transformations.map(_lastName) {
+        it != "" && !isNameValid(it)
+    }
+
+    private val _eventsFlow = MutableSharedFlow<RegistrationViewEvent>()
+    val eventsFlow = _eventsFlow.asSharedFlow()
+    private var registrationProcess: Job? = null
+
+
+    fun setBloodType(bloodType: String) {
+        _bloodType.value = BloodType.fromString(bloodType)
+    }
+
+    fun setBirthDate(birthDate: BirthDate) {
+        _birthDate.value = birthDate
+    }
+
+    fun setGender(gender: String) {
+        _gender.value = Gender.fromString(gender)
+    }
+
+    fun setAddress(address: Location?) {
+        _addressLiveData.value = address
+    }
+
+    fun observeFirstNameText(editText: Any) {
+        _firstName.value = (editText as EditText).text.toString()
+    }
+
+    fun observeLastNameText(editText: Any) {
+        _lastName.value = (editText as EditText).text.toString()
+    }
+
+    /** fun tryRegisterUser() {
+        if (areInputsValidAndComplete()) {
+            registrationProcess = registerUser(createUserRegistrationQuery())
+        } else {
+            viewModelScope.launch {
+                _eventsFlow.emit(RegistrationViewEvent.ShowSnackBarFromResource(R.string.fill_all_data))
+            }
+        }
+    }**/
+
+    private fun areInputsValidAndComplete(): Boolean {
+        return isNameValid(firstName.value!!) && isNameValid(lastName.value!!)
+                && isAddressValid() && isBirthDateValid()
+    }
+
+    private fun isNameValid(name: String) =
+        InputValidator.isValidNameEntered(name.removeAdditionalSpaces())
+
+    private fun isAddressValid() = addressLiveData.value != null
+    private fun isBirthDateValid() = _birthDate.value != null
+
+
+    private fun registerUser(registrationQuery: RegistrationQuery) = viewModelScope.launch {
+        _eventsFlow.emit(RegistrationViewEvent.ToggleLoadingIndicator)
+        try {
+            val registrationResponse = bloodDonationAPI.sendSMSRegistration(registrationQuery)
+            processRegistrationAPIResponse(registrationResponse)
+        } catch (e: Exception) {
+            _eventsFlow.emit(RegistrationViewEvent.ShowSnackBarFromResource(R.string.connection_error))
+        }
+        _eventsFlow.emit(RegistrationViewEvent.ToggleLoadingIndicator)
+    }
+
+    private suspend fun processRegistrationAPIResponse(registrationResponse: RegistrationResponse) {
+        if (registrationResponse.error != null) {
+            _eventsFlow.emit(RegistrationViewEvent.ShowSnackBarFromResource(registrationResponse.error.message.errorStringResource))
+        } else {
+            _eventsFlow.emit(RegistrationViewEvent.OpenSMSFragment)
+        }
+    }
+
+    fun cancelRegistrationProcess() {
+        registrationProcess?.cancel()
+    }
+
+    suspend fun getUserProfileData() {
+
+        // Get current profile information
+        val profileResponse = ProfileRepo.getUserProfileData(bloodDonationAPI, false)
+
+        // Set firstname
+        _firstName.postValue(profileResponse.user?.name?.firstName)
+
+        // Set lastname
+        _lastName.postValue(profileResponse.user?.name?.lastName)
+
+        // Set blood type
+        _bloodType.postValue(profileResponse.user?.bloodType!!)
+
+        // Set gender
+        //_gender.postValue(profileResponse.user.gender!!)
+
+        // Set birthdate
+        //_birthDate.postValue(profileResponse.user.birthDate)
+
+        // Set address
+        //_addressLiveData.postValue(profileResponse.user.location)
+    }
 }
